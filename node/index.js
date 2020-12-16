f2d25555ad797067ef2dc8c4f72f761a0786f6c0
hak0707 <40398044+hak0707@users.noreply.github.com>
2020-12-16 23:17:05 +0900

    FizzBuzzを作成する処理の実装

--- node/index.js
+++ node/index.js
@@ -11,7 +11,55 @@ const server = http.createServer((req, res)=>{
       });
    }
    if( endpoint==='/api' ){
-      // ここに処理を記述してください。  
+      // ここに処理を記述してください。
+
+      // note: apiのRequestPayloadをnode.jsで取得する処理と
+      //       ejsを使用せずにhtml要素を書き換える処理が分からなかった
+      //       学習して再チャレンジしたい
+
+      // TODO: Requestからobjを取得する処理
+      const request = {
+         obj: [
+            {
+               num: 4, text: "fizz"
+            },
+            {
+               num: 7, text: "buzz"
+            },
+            {
+               num: 8, text: "hoge"
+            },
+            {
+               num: 15, text: "huga"
+            }
+         ]
+      }
+
+      // 取得したrequestを元にFizzBuzzを配列にpushする処理
+      const n = 30;
+      const obj = request.obj;
+      const len = obj.length;
+      const result = [];
+
+      for (let i = 0; i < n; i++) {
+         let currentResult = '';
+         let currentNum = i + 1;
+
+         for (let j = 0; j < len; j++) {
+            if (currentNum % obj[j]['num'] === 0 ) {
+               currentResult = currentResult + obj[j]['text'];
+            }
+         }
+         if (currentResult === '') {
+            currentResult = currentNum;
+         }
+         result.push(currentResult);
+      }
+      console.log(result);
+
+      // TODO: index.htmlの<span id="result-text"></span>に
+      //       出力結果を展開する処理
+
    }
 });
 server.listen(8080); 
